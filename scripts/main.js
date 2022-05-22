655865fadf6a6c82605acc0e13000146e1fc934e
Anderson Vieira <asvieira.br@gmail.com>
2022-05-22 12:45:54 -0300

    New stylesheet

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home,
+  #home .stats,
+  #skills,
+  #skills header,
+  #blog,
+  #blog header,`)
